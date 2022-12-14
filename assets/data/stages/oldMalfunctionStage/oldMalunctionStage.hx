0e5ec25243563b49674c5d733e976c36ea7d426e
DEMOLITIONDON96 <87618246+DEMOLITIONDON96@users.noreply.github.com>
2022-12-14 11:05:26 -0600

    making a copy of Malfunction stage for legacy version

---
+++ assets/data/stages/oldMalfunctionStage/oldMalunctionStage.hx
@@ -0,0 +1,15 @@
+function onCreate()
+{
+	PlayState.defaultCamZoom = 0.8;
+	spawnGirlfriend(false);
+
+	var fuckingsquares:FNFSprite = new FNFSprite(0, 0).loadGraphic(Paths.image('PixelMouse', 'data/stages/forbiddenRealm/images'));
+	fuckingsquares.scale.set(1, 1);
+	fuckingsquares.updateHitbox();
+	fuckingsquares.antialiasing = false;
+	fuckingsquares.scrollFactor(1, 1);
+	fuckingsquares.active = false;
+	add(fuckingsquares);
+}
+
+//I'll add the offsets later
