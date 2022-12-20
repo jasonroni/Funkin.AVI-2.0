93e2fd2f372d6d5e7ee594e027699bbe1a071c2d
JustJasonLol <98182734+JustJasonLol@users.noreply.github.com>
2022-12-19 22:40:45 -0500

    Grr i hate seeing Character.hx not having the alpha thing

--- assets/data/stages/staticVoid/staticVoid.hx
+++ assets/data/stages/staticVoid/staticVoid.hx
@@ -1,7 +1,6 @@
 function onCreate()
 {
   spawnGirlfriend(false);
-  PlayState.boyfriend.alpha = 0;
   
   var whoaBlackBG:FlxSprite = new FlxSprite(0, 0).makeGraphic(2000, 2000, 0x000000);
   whoaBlackBG.screenCenter();
