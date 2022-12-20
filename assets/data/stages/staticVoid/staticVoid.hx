da76a80534c347a5e6f063e635c0251fa01b4213
JustJasonLol <98182734+JustJasonLol@users.noreply.github.com>
2022-12-19 22:48:21 -0500

    Adding the funny RS TV
    haven’t tested yet

--- assets/data/stages/staticVoid/staticVoid.hx
+++ assets/data/stages/staticVoid/staticVoid.hx
@@ -1,9 +1,19 @@
+var datTV:FlxSprite;
+
 function onCreate()
 {
   spawnGirlfriend(false);
   
+  var thePath:String = 'data/stages/staticVoid/images';
+  
   var whoaBlackBG:FlxSprite = new FlxSprite(0, 0).makeGraphic(2000, 2000, 0x000000);
   whoaBlackBG.screenCenter();
   add(whoaBlackBG);
+  
+  datTV = new FlxSprite().loadGraphic(Paths.getSparrowAtlas('white', thePath));
+  datTV.animation.addByPrefix('idle', 'white idle');
+  datTV.animation.play('idle');
+  datTV.alpha = 0;
+  add(datTV);
 }
     
