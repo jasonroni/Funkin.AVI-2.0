ad68ad607aee6a8d9a89cc0260fd5df1126e0fc7
DEMOLITIONDON96 <87618246+DEMOLITIONDON96@users.noreply.github.com>
2023-01-11 11:21:38 -0600

    haven't tested yet

--- assets/data/stages/forbiddenRealm/forbiddenRealm.hx
+++ assets/data/stages/forbiddenRealm/forbiddenRealm.hx
@@ -1,4 +1,5 @@
 import flixel.effects.particles.FlxEmitter;
+import flixel.effects.particles.FlxParticle;
 
 function onCreate()
 {
@@ -14,27 +15,48 @@ function onCreate()
 	add(fuckingsquares);
 
 	var greyParticles:FlxEmitter = new FlxEmitter(-2080.5, 650.4);
-    greyParticles.launchMode = FlxEmitterMode.SQUARE;
-    greyParticles.velocity.set(-50, -200, 50, -600, -90, 0, 90, -600);
-    greyParticles.scale.set(4, 4, 4, 4, 0, 0, 0, 0);
-    greyParticles.drag.set(0, 0, 0, 0, 5, 5, 10, 10);
-    greyParticles.width = 4787.45;
-    greyParticles.alpha.set(1, 1);
-    greyParticles.lifespan.set(1.9, 4.9);
-    greyParticles.loadParticles(Paths.image('greyParticle', 'data/stages/forbiddenRealm/images'), 500, 16, true);
-    greyParticles.start(false, FlxG.random.float(.0521, .1060), 1000000);
-    add(greyParticles);
+    	greyParticles.launchMode = FlxEmitterMode.SQUARE;
+    	greyParticles.velocity.set(-50, -200, 50, -600, -90, 0, 90, -600);
+    	greyParticles.scale.set(4, 4, 4, 4, 0, 0, 0, 0);
+    	greyParticles.drag.set(0, 0, 0, 0, 5, 5, 10, 10);
+    	greyParticles.width = 4787.45;
+    	greyParticles.alpha.set(1, 1);
+    	greyParticles.lifespan.set(1.9, 4.9);
+    	greyParticles.loadParticles(Paths.image('greyParticle', 'data/stages/forbiddenRealm/images'), 500, 16, true);
+    	greyParticles.start(false, FlxG.random.float(.0521, .1060), 1000000);
+    	add(greyParticles);
 
-    var blackParticles:FlxEmitter = new FlxEmitter(-2080.5, 1512.4);
-    blackParticles.launchMode = FlxEmitterMode.SQUARE;
-    blackParticles.velocity.set(-70, -220, 70, -620, -110, 20, 110, -620);
-    blackParticles.scale.set(6, 6, 6, 6, 2, 2, 2, 2);
-    blackParticles.drag.set(2, 2, 2, 2, 7, 7, 12, 12);
-    blackParticles.width = 4787.45;
-    blackParticles.alpha.set(1, 1);
-    blackParticles.lifespan.set(1.9, 4.9);
-    blackParticles.loadParticles(Paths.image('particleBlack', 'data/stages/forbiddenRealm/images'), 500, 16, true);
-    add(blackParticles);
+    	var blackParticles:FlxEmitter = new FlxEmitter(-2080.5, 1512.4);
+    	blackParticles.launchMode = FlxEmitterMode.SQUARE;
+    	blackParticles.velocity.set(-70, -220, 70, -620, -110, 20, 110, -620);
+    	blackParticles.scale.set(6, 6, 6, 6, 2, 2, 2, 2);
+    	blackParticles.drag.set(2, 2, 2, 2, 7, 7, 12, 12);
+    	blackParticles.width = 4787.45;
+    	blackParticles.alpha.set(1, 1);
+    	blackParticles.lifespan.set(1.9, 4.9);
+    	blackParticles.loadParticles(Paths.image('particleBlack', 'data/stages/forbiddenRealm/images'), 500, 16, true);
+    	foreground.add(blackParticles);
+	
+	var mickeyEmitter:FlxEmitter = new FlxEmitter(-2099.8, 1620.4);
+	for (i in 0 ... 100)
+	{
+		var mickeyParticle = new FlxParticle();
+		mickeyParticle.frames = Paths.getSparrowAtlas('mickParticle', 'data/stages/forbiddenRealm/images');
+		mickeyParticle.animation.addByPrefix('laugh', 'mickParticle idle', 19, true);
+		mickeyParticle.animation.play('laugh');
+		mickeyParticle.exists = false;
+		mickeyParticle.animation.curAnim.curFrame = FlxG.random.int(0, 3);
+		mickeyEmitter.add(mickeyParticle);
+	}
+	mickeyEmitter.launchMode = FlxEmitterMode.SQUARE;
+	mickeyEmitter.velocity.set(-50, -400, 50, -800, -100, 0, 100, -800);
+	mickeyEmitter.scale.set(3.4, 3.4, 3.4, 3.4, 0, 0, 0, 0);
+	mickeyEmitter.drag.set(0, 0, 0, 0, 5, 5, 10, 10);
+	mickeyEmitter.width = 4200.45;
+	mickeyEmitter.alpha.set(1, 1);
+	mickeyEmitter.lifespan.set(4, 4.5);
+	mickeyEmitter.start(false, FlxG.random.float(0.3, 0.4), 100000);
+	mickeyEmitter.emitting = false;
 	
 	//Imposter V4 code for later
 	/*heartEmitter = new FlxEmitter(-1200, 1000);
