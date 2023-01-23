cb782d483096b22b1eacefddb3afa60320088f5f
DEMOLITIONDON96 <87618246+DEMOLITIONDON96@users.noreply.github.com>
2023-01-23 14:37:51 -0600

    this is so much better than having to make a new stage

--- assets/data/stages/waltRoom/waltRoom.hx
+++ assets/data/stages/waltRoom/waltRoom.hx
@@ -1,6 +1,8 @@
 var gettingSleepy:FlxSprite;
 var limitThing:Int = 0; //Default Value
 
+var pissOfGlory:FlxSprite;
+
 var vhsFilter:FlxRuntimeShader;
 var grainFilter:FlxRuntimeShader;
 var monitorFilter:FlxRuntimeShader; //I can't get this one to work without covering the entire screen
@@ -13,7 +15,16 @@ function onCreate()
 	//PlayState.boyfriend.alpha = 0; //This crashes the game cause apparently, Character.hx doesn't have an "alpha" value.
 	PlayState.defaultCamZoom = 0.75;
 
-	var pissOfGlory:FNFSprite = new FNFSprite(-450, -300).loadGraphic(Paths.image('walt-bg', 'data/stages/waltRoom/images'));
+	if (PlayState.SONG.song == 'Mercy')
+	{
+		pissOfGlory = new FNFSprite(-470, -280);
+		pissOfGlory.loadGraphic(Paths.image('newWaltBG', 'data/stages/waltRoom/images'));
+		pissOfGlory.scale.set(1.7, 1.7);
+	}else{
+		pissOfGlory = new FNFSprite(-450, -300);
+		pissOfGlory.loadGraphic(Paths.image('walt-bg', 'data/stages/waltRoom/images'));
+		pissOfGlory.scale.set(1, 1);
+	}
 	pissOfGlory.scale.set(1, 1);
 	pissOfGlory.updateHitbox();
 	pissOfGlory.antialiasing = true;
