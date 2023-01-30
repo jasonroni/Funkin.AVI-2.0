d1280917e8ea182b81d12c8047972019161d0121
DEMOLITIONDON96 <87618246+DEMOLITIONDON96@users.noreply.github.com>
2023-01-30 10:53:33 -0600

    3 stages done

--- assets/data/stages/staticVoid/staticVoid.hx
+++ assets/data/stages/staticVoid/staticVoid.hx
@@ -1,8 +1,21 @@
 var datTV:FlxSprite;
 
+var holyShitStatic:FlxRuntimeShader;
+
+var shaderTime:Float = 0;
+
 function onCreate()
 {
   spawnGirlfriend(false);
+	PlayState.defaultCamZoom = 0.85;
+
+	holyShitStatic = new FlxRuntimeShader(File.getContent('./assets/shaders/tvStatic.frag', null, 120));
+
+	PlayState.camGame.setFilters(
+	[
+		new ShaderFilter(holyShitStatic)
+	]);
+	
   
   var thePath:String = 'data/stages/staticVoid/images';
   
@@ -10,10 +23,29 @@ function onCreate()
   whoaBlackBG.screenCenter();
   add(whoaBlackBG);
   
-  datTV = new FlxSprite().loadGraphic(Paths.getSparrowAtlas('white', thePath));
+  datTV = new FlxSprite(-250, -160);
+	datTV.frames = Paths.getSparrowAtlas('white', thePath);
   datTV.animation.addByPrefix('idle', 'white idle');
   datTV.animation.play('idle');
-  datTV.alpha = 0;
+	datTV.scale.set(0.6, 0.6);
+  datTV.alpha = 0.001;
   add(datTV);
+}
+
+function onBeat(curBeat:Int, boyfriend:Character, gf:Character, dad:Character)
+{
+	if (curBeat == 104)
+	{
+		datTV.alpha = 1;
+		PlayState.camGame.flash(ForeverTools.returnColor("white"), 1);
+	}
+}
+
+function onUpdate(elapsed:Float, boyfriend:Character, gf:Character, dad:Character)
+{
+    shaderTime += elapsed;
+
+    holyShitStatic.setFloat('uTime', shaderTime);
+    holyShitStatic.setFloat('iTime', shaderTime);
 }
     
--- assets/data/stages/waltRoom/waltRoom.hx
+++ assets/data/stages/waltRoom/waltRoom.hx
@@ -1,3 +1,4 @@
+import flixel.text.FlxText;
 import openfl.filters.ShaderFilter;
 
 var gettingSleepy:FlxSprite;
@@ -6,8 +7,13 @@ var limitThing:Int = 0; //Default Value
 var pissOfGlory:FNFSprite;
 var greaterPiss:FNFSprite;
 
+var inkFormWarning:FlxText;
+
+var spaceBarCounter:FlxText;
+
 var vhsFilter:FlxRuntimeShader;
 var grainFilter:FlxRuntimeShader;
+var monitorFilter:FlxRuntimeShader; //I can't get this one to work without covering the entire screen
 var shaderTime:Float = 0;
 
 function onCreate()
@@ -17,11 +23,11 @@ function onCreate()
 	//PlayState.boyfriend.alpha = 0; //This crashes the game cause apparently, Character.hx doesn't have an "alpha" value.
 	PlayState.defaultCamZoom = 0.75;
 
-
 	if (PlayState.SONG.song == 'Mercy')
 	{
 		PlayState.camGame.alpha = 0;
 		PlayState.camHUD.alpha = 0;
+		//PlayState.dadStrums.visible = false;
 
 		pissOfGlory = new FNFSprite(-470, -280);
 		pissOfGlory.loadGraphic(Paths.image('newWaltBG', 'data/stages/waltRoom/images'));
@@ -50,7 +56,14 @@ function onCreate()
 	vignette.scrollFactor();
 	vignette.active = false;
 	add(vignette);
-	
+
+	gettingSleepy = new FlxSprite(-FlxG.width * FlxG.camera.zoom,
+		-FlxG.height * FlxG.camera.zoom).makeGraphic(FlxG.width * 3, FlxG.height * 3, 0xFF000000);
+	gettingSleepy.scrollFactor.set();
+	gettingSleepy.cameras = [PlayState.camAlt];
+	gettingSleepy.alpha = 0; //Default value
+	add(gettingSleepy);
+
 	var waltInstructionsMain:FlxText = new FlxText(370, 500, 0, "Take Advantage of the SPACEBAR!", 30);
 	waltInstructionsMain.cameras = [PlayState.camAlt];
 	waltInstructionsMain.setFormat(Paths.font("splatter"), 30);
@@ -59,7 +72,7 @@ function onCreate()
 	waltInstructionsMain.scrollFactor.set();
 	add(waltInstructionsMain);
 
-	var waltSubTxt:FlxText = new FlxText(waltInstructionsMain.x + 85, waltInstructionsMain.y + 40, 0, "(It Will Help You Regain Health When In Need)", 15);
+	var waltSubTxt:FlxText = new FlxText(waltInstructionsMain.x + 66, waltInstructionsMain.y + 40, 0, "(It will help you regain health when critically low)", 15);
 	waltSubTxt.setFormat(Paths.font("splatter"), 15);
 	waltSubTxt.cameras = [PlayState.camAlt];
 	waltSubTxt.borderSize = 2;
@@ -67,26 +80,30 @@ function onCreate()
 	waltSubTxt.alpha = 0;
 	waltSubTxt.scrollFactor.set();
 	add(waltSubTxt);
-	
-	gettingSleepy = new FlxSprite(-FlxG.width * FlxG.camera.zoom,
-		-FlxG.height * FlxG.camera.zoom).makeGraphic(FlxG.width * 3, FlxG.height * 3, 0xFF000000);
-	gettingSleepy.scrollFactor.set();
-	gettingSleepy.cameras = [PlayState.camAlt];
-	gettingSleepy.alpha = 0; //Default value
-	add(gettingSleepy);
 
+	inkFormWarning = new FlxText(0, 0, 0, "PRESS SPACE!", 15);
+	inkFormWarning.setFormat(Paths.font("splatter"), 50);
+	inkFormWarning.cameras = [PlayState.camAlt];
+	inkFormWarning.alpha = 0;
+	inkFormWarning.scrollFactor.set();
+	inkFormWarning.screenCenter();
+	add(inkFormWarning);
+
+	spaceBarCounter = new FlxText(0, 680, 0, 'Health Boosts Left: ' + limitThing, 15);
+	spaceBarCounter.setFormat(Paths.font("splatter"), 30);
+	spaceBarCounter.cameras = [PlayState.camAlt];
+	spaceBarCounter.scrollFactor.set();
+	add(spaceBarCounter);
 
 	FlxTween.tween(waltInstructionsMain, {alpha: 0}, 1, {ease: FlxEase.quadInOut, startDelay: 8});
 	FlxTween.tween(waltSubTxt, {alpha: 0}, 1, {ease: FlxEase.quadInOut, startDelay: 8});
 	FlxTween.tween(waltSubTxt, {alpha: 1}, 0.7, {ease: FlxEase.quadInOut, startDelay: 3});
 
-	if (PlayState.SONG.song == 'Mercy Legacy') limitThing += 12; else if (PlayState.SONG.song == 'Mercy') limitThing += 8; else limitThing += 5; //This line sets up the limit based on the song it's set on.
+	if (PlayState.SONG.song == 'Mercy Legacy') limitThing += 12; else if (PlayState.SONG.song == 'Mercy') limitThing += 16; else limitThing += 5; //This line sets up the limit based on the song it's set on.
 
 	vhsFilter = new FlxRuntimeShader(File.getContent("./assets/shaders/vhs.frag"), null, 130);
-	vhsFilter.setFloat('time', 0);
 
 	grainFilter = new FlxRuntimeShader(File.getContent("./assets/shaders/filmgrain.frag"), null, 150);
-	grainFilter.setFloat('time', 0.0);
 
 	PlayState.camGame.setFilters(
 		[
@@ -113,30 +130,69 @@ function onBeat(curBeat:Int, boyfriend:Character, gf:Character, dad:Character)
 		else if (curBeat >= 256 && curBeat <= 319) PlayState.health -= 0.18;
 		else if (curBeat >= 320) PlayState.health -= 0.01;
 	}else if (PlayState.SONG.song == 'Mercy'){
-		if (curBeat >= 0 && curBeat <= 79) PlayState.health -= 0.005;
-		if (curBeat >= 256 && curBeat <= 275) PlayState.health -= 0.005;
-		if (curBeat >= 88 && curBeat <= 95) PlayState.health -= 0.005;
-		if (curBeat >= 80 && curBeat <= 87) PlayState.health -= 0.03;
-		if (curBeat >= 96 && curBeat <= 127) PlayState.health -= 0.01;
-		if (curBeat >= 192 && curBeat <= 239) PlayState.health -= 0.01;
-		if (curBeat >= 128 && curBeat <= 159) PlayState.health -= 0.05;
-		if (curBeat >= 160 && curBeat <= 191) PlayState.health -= 0.08;
-		if (curBeat >= 240 && curBeat <= 255) PlayState.health -= 0.1;
-		if (curBeat >= 276) PlayState.health -= 0.11;
-		if (curBeat == 8)
+
+		// Walt's Health Mechanic
+		if (curBeat >= 0 && curBeat <= 63) PlayState.health -= 0.005;
+		else if (curBeat >= 64 && curBeat <= 79) PlayState.health -= 0.01;
+		else if (curBeat >= 80 && curBeat <= 87) PlayState.health -= 0.07;
+		else if (curBeat >= 88 && curBeat <= 95) PlayState.health -= 0.01;
+		else if (curBeat >= 96 && curBeat <= 127) PlayState.health -= 0.03;
+		else if (curBeat >= 128 && curBeat <= 159) PlayState.health -= 0.1;
+		else if (curBeat >= 160 && curBeat <= 191) PlayState.health -= 0.06;
+		else if (curBeat >= 192 && curBeat <= 207) PlayState.health -= 0.01;
+		else if (curBeat >= 208 && curBeat <= 239) PlayState.health -= 0.04;
+		else if (curBeat >= 240 && curBeat <= 255) PlayState.health -= 0.005;
+		else if (curBeat >= 256 && curBeat <= 291) PlayState.health -= 0.03;
+		else if (curBeat >= 292 && curBeat <= 307) PlayState.health -= 0.05;
+		else if (curBeat >= 308 && curBeat <= 339) PlayState.health -= 0.085;
+		else if (curBeat >= 340 && curBeat <= 371) PlayState.health -= 0.1;
+		else if (curBeat >= 372 && curBeat <= 387) PlayState.health -= 0.12;
+		else if (curBeat >= 388 && curBeat <= 403) PlayState.health -= 0.135;
+		else if (curBeat >= 404 && curBeat <= 451) PlayState.health -= 0.15;
+		else if (curBeat >= 452 && curBeat <= 467) PlayState.health -= 0.2;
+		else if (curBeat >= 468 && curBeat <= 475) PlayState.health -= 0.25;
+		else if (curBeat >= 476 && curBeat <= 489) PlayState.health -= 0.3;
+		else if (curBeat >= 490) PlayState.health -= 0.02;
+
+		// Mid-Song Stuff
+		if (curBeat == 16)
 		{
 			FlxTween.tween(PlayState.camGame, {alpha: 1}, 5, {ease: FlxEase.sineInOut});
 			FlxTween.tween(PlayState.camHUD, {alpha: 1}, 5, {ease: FlxEase.sineInOut, startDelay: 1.5});
+			PlayState.defaultCamZoom = 1.3;
 		}
+		if (curBeat == 32) PlayState.defaultCamZoom = 1.2;
+		if (curBeat == 40) PlayState.defaultCamZoom = 1.1;
+		if (curBeat == 48) PlayState.defaultCamZoom = 1;
+		if (curBeat == 56) PlayState.defaultCamZoom = 0.9;
+		if (curBeat == 64) PlayState.defaultCamZoom = 0.75;
 		if (curBeat == 256)
 		{
 			FlxTween.tween(PlayState.camGame, {alpha: 0}, 1, {ease: FlxEase.sineInOut});
 		}
-		if (curBeat == 264) FlxTween.tween(PlayState.camGame, {alpha: 1}, 2, {ease: FlxEase.sineInOut});
+		if (curBeat == 264)
+		{
+			FlxTween.tween(PlayState.camGame, {alpha: 1}, 2, {ease: FlxEase.sineInOut});
+			PlayState.defaultCamZoom = 1.3;
+		}
 		if (curBeat == 275)
 		{
-			FlxTween.tween(greaterPiss, {alpha: 1}, 0.3, {ease: FlxEase.quartInOut});
+			PlayState.defaultCamZoom = 0.8;
+			inkFormWarning.alpha = 1;
+			FlxTween.tween(pissOfGlory, {alpha: 0}, 0.5, {ease: FlxEase.quartInOut});
+			FlxTween.tween(greaterPiss, {alpha: 1}, 0.5, {ease: FlxEase.quartInOut});
 		}
+		if (curBeat == 276) FlxTween.tween(inkFormWarning, {alpha: 0}, 2, {ease: FlxEase.sineInOut});
+		if (curBeat == 494)
+		{
+			PlayState.camHUD.flash(ForeverTools.returnColor("white"), 3);
+			PlayState.camGame.visible = 0;
+			FlxTween.tween(PlayState.bfStrums, {alpha: 0}, 1, {ease: FlxEase.sineInOut});
+			FlxTween.tween(PlayState.camHUD, {alpha: 0}, 1, {ease: FlxEase.sineInOut, startDelay: 3});
+			FlxTween.tween(spaceBarCounter, {alpha: 0}, 2, {ease: FlxEase.sineInOut});
+		}
+		if (curBeat == 501) PlayState.bfStrums.visible = false;
+
 	}else{
 		PlayState.health -= 0.07; //Default drain value
 	}
@@ -148,6 +204,8 @@ function onUpdate(elapsed:Float, boyfriend:Character, gf:Character, dad:Characte
 	shaderTime += elapsed;
 	vhsFilter.setFloat('time', shaderTime);
 	grainFilter.setFloat('time', shaderTime);
+
+	spaceBarCounter.text = 'Health Boosts Left: ' + limitThing;
 
 	//This entire set monitors the brightness of the screen based on the percentage of your health
 	
@@ -220,7 +278,7 @@ function onUpdate(elapsed:Float, boyfriend:Character, gf:Character, dad:Characte
 	As you see at the top, it's at 0, but in the onCreate function, you can see it adds a certain amount based on the song given (by default, it's 5).
 	The moment it reaches 0, you will no longer be able to use the spacebar key, encouraging players to actually use it wisely.
 	*/
-	if(FlxG.keys.justPressed.SPACE && PlayState.health < 0.25 && limitThing > 0)
+	if(FlxG.keys.justPressed.SPACE && PlayState.health < 0.3 && limitThing > 0)
 	{
 		PlayState.health += 1.25;
 		limitThing -= 1;
