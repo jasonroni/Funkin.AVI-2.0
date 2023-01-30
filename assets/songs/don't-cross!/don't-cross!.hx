74fe63180a29f31883e266b728f4581d6fd30d62
JustJasonLol <98182734+JustJasonLol@users.noreply.github.com>
2023-01-30 14:12:03 -0300

    1 of the quantillion dont cross mechanics

---
+++ assets/songs/don't-cross!/don't-cross!.hx
@@ -0,0 +1,15 @@
+function goodNoteHit()
+   {
+      PlayState.boyfriend.x -= 1.2;
+      PlayState.boyfriend.y += 1.2;
+      PlayState.boyfriend.scale.x += 0.0012;
+      PlayState.boyfriend.scale.y += 0.0012;
+   }
+
+function opponentNoteHit()
+   {
+      PlayState.boyfriend.x += 1.2;
+      PlayState.boyfriend.y -= 1.2;
+      PlayState.boyfriend.scale.x -= 0.0012;
+      PlayState.boyfriend.scale.y -= 0.0012;
+   }
