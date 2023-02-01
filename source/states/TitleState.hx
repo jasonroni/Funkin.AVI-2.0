8fb8e4c188c7401761da02810eb228e227c43082
DEMOLITIONDON96 <87618246+DEMOLITIONDON96@users.noreply.github.com>
2023-02-01 10:20:00 -0600

    fixed up the locker

--- source/states/TitleState.hx
+++ source/states/TitleState.hx
@@ -94,7 +94,7 @@ class TitleState extends states.MusicBeatState
 
 	override public function create():Void
 	{	
-		if (FlxG.save.data.episode1FPLock == null) GameData.lockinIt(); // this locks everything to the default state if the player hasn't played it yet, leaving only Episode 1 unlocked
+		if (FlxG.save.data.episode1FPLock == null || FlxG.save.data.episodeSFPLock == null /*an alternative in case we don't make a new save for the testers*/) GameData.lockinIt(); // this locks everything to the default state if the player hasn't played it yet, leaving only Episode 1 unlocked
 
 		FlxG.game.focusLostFramerate = 60;
 		FlxG.sound.muteKeys = muteKeys;
@@ -123,7 +123,7 @@ class TitleState extends states.MusicBeatState
 
 		#if OnWindows
 		base.system.CppAPI.darkMode();
-        #end
+        	#end
 
 
 
