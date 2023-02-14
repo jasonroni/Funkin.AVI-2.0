3b40277ac314040fbe17086ece8a90117002e22c
DEMOLITIONDON96 <87618246+DEMOLITIONDON96@users.noreply.github.com>
2023-02-14 09:12:32 -0600

    fucking typos, grah

--- source/states/menus/story/MainStoryState.hx
+++ source/states/menus/story/MainStoryState.hx
@@ -11,7 +11,7 @@ import flixel.text.FlxText;
 import flixel.tweens.FlxTween;
 import flixel.util.FlxColor;
 import flixel.util.FlxTimer;
-import ibjects.ui.menu.*;
+import objects.ui.menu.*;
 import states.MusicBeatState;
 import base.dependency.Discord;
 
