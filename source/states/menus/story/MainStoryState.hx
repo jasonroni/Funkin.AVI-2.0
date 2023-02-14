35f62cea11d797046d574b4caaf1bb047cd00d03
DEMOLITIONDON96 <87618246+DEMOLITIONDON96@users.noreply.github.com>
2023-02-14 10:13:52 -0600

    oops

--- source/states/menus/story/MainStoryState.hx
+++ source/states/menus/story/MainStoryState.hx
@@ -278,14 +278,6 @@ class MainStoryState extends MusicBeatState
 		intendedScore = ScoreUtils.getWeekScore(curWeek, curDifficulty);
 
 		FlxTween.tween(sprDifficulty, {y: leftArrow.y + 15, alpha: 1}, 0.07);
-
-		sprDifficulty.alpha = 0;
-
-		// USING THESE WEIRD VALUES SO THAT IT DOESNT FLOAT UP
-		sprDifficulty.y = leftArrow.y - 15;
-		intendedScore = Highscore.getWeekScore(curWeek, curDifficulty);
-
-		FlxTween.tween(sprDifficulty, {y: leftArrow.y + 15, alpha: 1}, 0.07);
 	}
 
 	var lerpScore:Int = 0;
