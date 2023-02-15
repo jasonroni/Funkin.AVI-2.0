ab3065d3d0aa50f70646a4e6e464dab74dbe24a6
DEMOLITIONDON96 <87618246+DEMOLITIONDON96@users.noreply.github.com>
2023-02-15 13:53:22 -0600

    I'll make a new RPC with all these assets soon so it won't break

--- source/states/menus/MainMenu.hx
+++ source/states/menus/MainMenu.hx
@@ -92,10 +92,6 @@ class MainMenu extends MusicBeatState
 		transIn = FlxTransitionableState.defaultTransIn;
 		transOut = FlxTransitionableState.defaultTransOut;
 
-		#if DISCORD_RPC
-		Discord.changePresence('MENU SCREEN', 'Main Menu');
-		#end
-
 		// uh
 		persistentUpdate = persistentDraw = true;
 
@@ -290,6 +286,10 @@ class MainMenu extends MusicBeatState
 
 		var lerpVal:Float = CoolUtil.boundTo(elapsed * 7.5, 0, 1);
 		camFollowPos.setPosition(FlxMath.lerp(camFollowPos.x, camFollow.x, lerpVal), FlxMath.lerp(camFollowPos.y, camFollow.y, lerpVal));
+		
+		#if DISCORD_RPC
+		Discord.changePresence('MAIN MENU', 'Selecting: ' + optionShit[Math.floor(curSelected)], 'icon', 'mouse');
+		#end
 
 		if ((controlArray.contains(true)) && (!selectedSomethin))
 		{
