f9271a2e202d5f24226b2fb269e90011271615fc
DEMOLITIONDON96 <87618246+DEMOLITIONDON96@users.noreply.github.com>
2023-02-16 09:35:02 -0600

    once again, experimenting with shit that probably isn't gonna work

--- source/objects/ui/HealthIcon.hx
+++ source/objects/ui/HealthIcon.hx
@@ -11,6 +11,10 @@ class HealthIcon extends FlxSprite
 	public var sprTracker:FlxSprite;
 	public var initialWidth:Float = 0;
 	public var initialHeight:Float = 0;
+	
+	// Dynamic Icon support in my way cause this engine's original code didn't work anyways
+	public var frames:Array<Int> = [0, 1, 2, 3, 4];
+	public var finalWidth = 5;
 
 	public var canBounce:Bool = true;
 	public var suffix:String = '';
@@ -31,12 +35,34 @@ class HealthIcon extends FlxSprite
 
 	public dynamic function updateAnim(health:Float)
 	{
-		if (health > 80)
-			animation.curAnim.curFrame = 2;
-		if (health < 20)
-			animation.curAnim.curFrame = 1;
-		else
-			animation.curAnim.curFrame = 0;
+		switch (finalWidth)
+		{
+			case 5:
+				if (health > 90)
+					animation.curAnim.curFrame = 4;
+				else if (health > 80)
+					animation.curAnim.curFrame = 2;
+				if (health < 20)
+					animation.curAnim.curFrame = 1;
+				else if (health < 10)
+					animation.curAnim.curFrame = 3;
+				else
+					animation.curAnim.curFrame = 0;
+			case 3:
+				if (health > 80)
+					animation.curAnim.curFrame = 2;
+				if (health < 20)
+					animation.curAnim.curFrame = 1;
+				else
+					animation.curAnim.curFrame = 0;
+			case 2:
+				if (health < 20)
+					animation.curAnim.curFrame = 1;
+				else
+					animation.curAnim.curFrame = 0;
+			case 1:
+				animation.curAnim.curFrame = 0;
+		}
 	}
 
 	public function bop(framerate:Float)
@@ -64,15 +90,35 @@ class HealthIcon extends FlxSprite
 				iconPath = 'placeholder';
 		}
 
-		antialiasing = true;
+		if (suffix == '-pixel')
+			antialiasing = false;
+		else
+			antialiasing = true;
 
-		var iconGraphic:FlxGraphic = Paths.image('$iconPath/icon$suffix', 'data/characters');
-		loadGraphic(iconGraphic, true, Std.int(iconGraphic.width / 3), iconGraphic.height);
+		var iconGraphic:Dynamic = Paths.image('$iconPath/icon$suffix', 'data/characters');
+		
+		loadGraphic(iconGraphic);
+		switch (iconGraphic.width)
+		{
+			case 750:
+				finalWidth = 5;
+				frames = [0, 1, 2, 3, 4];
+			case 450:
+				finalWidth = 3;
+				frames = [0, 1, 2];
+			case 300:
+				finalWidth = 2;
+				frames = [0, 1];
+			case 150:
+				finalWidth = 1;
+				frames = [0];
+		}
+		loadGraphic(iconGraphic, true, Std.int(iconGraphic.width / finalWidth), iconGraphic.height);
 
 		initialWidth = width;
 		initialHeight = height;
 
-		animation.add('icon', [0, 1, 2], 0, false, isPlayer);
+		animation.add('icon', frames, 0, false, isPlayer);
 		animation.play('icon');
 		scrollFactor.set();
 	}
