6ec321ca1fa5de32a3b8af98cb36a06b4690b276
DEMOLITIONDON96 <87618246+DEMOLITIONDON96@users.noreply.github.com>
2023-02-21 10:52:30 -0600

    Hardcoded Walt's mechanics since we know it works now

--- source/states/PlayState.hx
+++ source/states/PlayState.hx
@@ -207,6 +207,11 @@ class PlayState extends MusicBeatState
 
 	public var scratch:FlxSprite; // Peter Griffin: This reminds me of the time I met the Scratch cat
 	public var scratchButLessVisible:FlxSprite;
+	
+	// Waltah, we need to cook.
+	var inkFormWarning:FlxText;
+	var spaceBarCounter:FlxText;
+	var limitThing:Int = 0; //Default Value
 
 	var fade:FlxSprite;
 	
@@ -721,10 +726,30 @@ class PlayState extends MusicBeatState
 		add(scratch);
 
 		fade = new FlxSprite().makeGraphic(FlxG.width * 3, FlxG.height * 3, 0x000000);
-		fade.screenCenter();
+		fade.scrollFactor.set();
 		fade.cameras = [camAlt];
 		fade.alpha = 0;
 		add(fade);
+		
+		inkFormWarning = new FlxText(0, 0, 0, "PRESS SPACE!", 15);
+		inkFormWarning.setFormat(Paths.font("splatter"), 50);
+		inkFormWarning.cameras = [camAlt];
+		inkFormWarning.alpha = 0;
+		inkFormWarning.scrollFactor.set();
+		inkFormWarning.screenCenter();
+		add(inkFormWarning);
+		
+		spaceBarCounter = new FlxText(0, 680, 0, 'Health Boosts Left: ' + limitThing, 15);
+		spaceBarCounter.setFormat(Paths.font("splatter"), 30);
+		spaceBarCounter.cameras = [camAlt];
+		spaceBarCounter.alpha = 0;
+		spaceBarCounter.scrollFactor.set();
+		add(spaceBarCounter);
+		
+		if (SONG.song == 'Mercy Legacy') 
+			limitThing += 23; 
+		else if (SONG.song == 'Mercy') 
+			limitThing += 16;
 
 		// call the funny intro cutscene depending on the song
 		songCutscene(false);
@@ -905,6 +930,88 @@ class PlayState extends MusicBeatState
 		stageBuild.stageUpdateConstant(elapsed, boyfriend, gf, opponent);
 
 		super.update(elapsed);
+		
+		if (curStage == 'waltRoom')
+		{
+			spaceBarCounter.text = 'Health Boosts Left: ' + limitThing;
+
+			//This entire set monitors the brightness of the screen based on the percentage of your health
+
+			if(health <= 0.1) //if 5% HP
+			{
+				FlxTween.tween(fade, {alpha: 0.95}, 0.15, {ease: FlxEase.sineInOut});
+			}else if (health <= 0.2) //if 10% HP
+			{
+				FlxTween.tween(fade, {alpha: 0.9}, 0.15, {ease: FlxEase.sineInOut});
+			}else if (health <= 0.3) //if 15% HP
+			{
+				FlxTween.tween(fade, {alpha: 0.85}, 0.15, {ease: FlxEase.sineInOut});
+			}else if (health <= 0.4) //if 20% HP (you get the idea)
+			{
+				FlxTween.tween(fade, {alpha: 0.8}, 0.15, {ease: FlxEase.sineInOut});
+			}else if (health <= 0.5)
+			{
+				FlxTween.tween(fade, {alpha: 0.75}, 0.15, {ease: FlxEase.sineInOut});
+			}else if (health <= 0.6)
+			{
+				FlxTween.tween(fade, {alpha: 0.7}, 0.15, {ease: FlxEase.sineInOut});
+			}else if (health <= 0.7)
+			{
+				FlxTween.tween(fade, {alpha: 0.65}, 0.15, {ease: FlxEase.sineInOut});
+			}else if (health <= 0.8)
+			{
+				FlxTween.tween(fade, {alpha: 0.6}, 0.15, {ease: FlxEase.sineInOut});
+			}else if (health <= 0.9)
+			{
+				FlxTween.tween(fade, {alpha: 0.55}, 0.15, {ease: FlxEase.sineInOut});
+			}else if (health <= 1)
+			{
+				FlxTween.tween(fade, {alpha: 0.5}, 0.15, {ease: FlxEase.sineInOut});
+			}else if (health <= 1.1)
+			{
+				FlxTween.tween(fade, {alpha: 0.45}, 0.15, {ease: FlxEase.sineInOut});
+			}else if (health <= 1.2)
+			{
+				FlxTween.tween(fade, {alpha: 0.4}, 0.15, {ease: FlxEase.sineInOut});
+			}else if (health <= 1.3)
+			{
+				FlxTween.tween(fade, {alpha: 0.35}, 0.15, {ease: FlxEase.sineInOut});
+			}else if (health <= 1.4)
+			{
+				FlxTween.tween(fade, {alpha: 0.3}, 0.15, {ease: FlxEase.sineInOut});
+			}else if (health <= 1.5)
+			{
+				FlxTween.tween(fade, {alpha: 0.25}, 0.15, {ease: FlxEase.sineInOut});
+			}else if (health <= 1.6)
+			{
+				FlxTween.tween(fade, {alpha: 0.2}, 0.15, {ease: FlxEase.sineInOut});
+			}else if (health <= 1.7)
+			{
+				FlxTween.tween(fade, {alpha: 0.15}, 0.15, {ease: FlxEase.sineInOut});
+			}else if (health <= 1.8)
+			{
+				FlxTween.tween(fade, {alpha: 0.1}, 0.15, {ease: FlxEase.sineInOut});
+			}else if (health <= 1.9)
+			{
+				FlxTween.tween(fade, {alpha: 0.05}, 0.15, {ease: FlxEase.sineInOut});
+			}else if (health <= 2)
+			{
+				FlxTween.tween(fade, {alpha: 0}, 0.15, {ease: FlxEase.sineInOut});
+			}
+
+			/*
+			Okay, time to explain how the spacebar works, basically, you can't spam it anymore unlike the Psych counterpart.
+			With the conditions given, the spacebar becomes available and accessible when your HP is at exactly 12.5% health or lower.
+			Seems simple, right? Well, people can easily exploit this if there is no limit given, so that's where the "limitThing" variable comes in.
+			As you see at the top, it's at 0, but in the onCreate function, you can see it adds a certain amount based on the song given (by default, it's 5).
+			The moment it reaches 0, you will no longer be able to use the spacebar key, encouraging players to actually use it wisely.
+			*/
+			if(FlxG.keys.justPressed.SPACE && health < 0.3 && limitThing > 0)
+			{
+				health += 1.25;
+				limitThing -= 1;
+			}
+		}
 
 		if (health > 2)
 			health = 2;
@@ -1962,6 +2069,90 @@ class PlayState extends MusicBeatState
 				coolNote.beatHit(curBeat);
 			});
 		}
+		
+		// Mechanics we don't want peeps to actually modify when snooping through the release files
+		switch (SONG.song)
+		{
+			case 'Scrapped':
+				switch (curBeat)
+				{
+					case 64: FlxTween.tween(opponent, {alpha: 1}, 10);
+					case 424: FlxTween.tween(opponent, {alpha: 0}, 5);
+				}
+				
+			case 'Mercy Legacy':
+				if (curBeat >= 0 && curBeat <= 63) PlayState.health -= 0.02;
+				else if (curBeat >= 64 && curBeat <= 95) PlayState.health -= 0.2;
+				else if (curBeat >= 96 && curBeat <= 127) PlayState.health -= 0.06;
+				else if (curBeat >= 128 && curBeat <= 191) PlayState.health -= 0.16;
+				else if (curBeat >= 192 && curBeat <= 255) PlayState.health -= 0.1;
+				else if (curBeat >= 256 && curBeat <= 319) PlayState.health -= 0.18;
+				else if (curBeat >= 320) PlayState.health -= 0.01;
+				
+			case 'Mercy':
+				// Cam Stuff Handler
+				switch (curBeat)
+				{
+					case 16:
+						FlxTween.tween(camGame, {alpha: 1}, 5, {ease: FlxEase.sineInOut});
+						FlxTween.tween(camHUD, {alpha: 1}, 5, {ease: FlxEase.sineInOut, startDelay: 1.5});
+						defaultCamZoom = 1.3;
+				
+					case 32: defaultCamZoom = 1.2;
+					case 40: defaultCamZoom = 1.1;
+					case 48: defaultCamZoom = 1;
+					case 56: defaultCamZoom = 0.9;
+					case 64: defaultCamZoom = 0.75;
+				
+					// Very Spooky Phase 2 Walt (real)
+					case 256: FlxTween.tween(camGame, {alpha: 0}, 1, {ease: FlxEase.sineInOut});
+				
+					case 264:
+						FlxTween.tween(camGame, {alpha: 1}, 2, {ease: FlxEase.sineInOut});
+						defaultCamZoom = 1.3;
+				
+					case 275:
+						defaultCamZoom = 0.8;
+						inkFormWarning.alpha = 1;
+				
+					case 276: FlxTween.tween(inkFormWarning, {alpha: 0}, 2, {ease: FlxEase.sineInOut});
+				
+					// Final Stretch
+					case 494:
+						camHUD.flash(ForeverTools.returnColor("white"), 3);
+						camGame.visible = false;
+						FlxTween.tween(bfStrums, {alpha: 0}, 1, {ease: FlxEase.sineInOut});
+						FlxTween.tween(camHUD, {alpha: 0}, 1, {ease: FlxEase.sineInOut, startDelay: 3});
+						FlxTween.tween(spaceBarCounter, {alpha: 0}, 2, {ease: FlxEase.sineInOut});
+				
+					case 500: bfStrums.visible = false;
+						
+				}
+				
+				// Health Drain Shit
+				if (curBeat >= 0 && curBeat <= 63) PlayState.health -= 0.005;
+				else if (curBeat >= 64 && curBeat <= 79) PlayState.health -= 0.01;
+				else if (curBeat >= 80 && curBeat <= 87) PlayState.health -= 0.07;
+				else if (curBeat >= 88 && curBeat <= 95) PlayState.health -= 0.01;
+				else if (curBeat >= 96 && curBeat <= 127) PlayState.health -= 0.03;
+				else if (curBeat >= 128 && curBeat <= 159) PlayState.health -= 0.1;
+				else if (curBeat >= 160 && curBeat <= 191) PlayState.health -= 0.06;
+				else if (curBeat >= 192 && curBeat <= 207) PlayState.health -= 0.01;
+				else if (curBeat >= 208 && curBeat <= 239) PlayState.health -= 0.04;
+				else if (curBeat >= 240 && curBeat <= 255) PlayState.health -= 0.005;
+				else if (curBeat >= 256 && curBeat <= 291) PlayState.health -= 0.03;
+				else if (curBeat >= 292 && curBeat <= 307) PlayState.health -= 0.05;
+				else if (curBeat >= 308 && curBeat <= 339) PlayState.health -= 0.085;
+				else if (curBeat >= 340 && curBeat <= 371) PlayState.health -= 0.1;
+				else if (curBeat >= 372 && curBeat <= 387) PlayState.health -= 0.12;
+				else if (curBeat >= 388 && curBeat <= 403) PlayState.health -= 0.135;
+				else if (curBeat >= 404 && curBeat <= 451) PlayState.health -= 0.15;
+				else if (curBeat >= 452 && curBeat <= 467) PlayState.health -= 0.2;
+				else if (curBeat >= 468 && curBeat <= 475) PlayState.health -= 0.25;
+				else if (curBeat >= 476 && curBeat <= 489) PlayState.health -= 0.3;
+				else if (curBeat >= 490) PlayState.health -= 0.02;
+			
+		}
 
 		callFunc('beatHit', [curBeat]);
 	}
@@ -1979,10 +2170,6 @@ class PlayState extends MusicBeatState
 				camDisplaceY = 0;
 			}
 		}
-		
-		if (SONG.song == 'Scrapped' && curSection == 16)
-			FlxTween.tween(opponent, {alpha: 1}, 10);
-			
 
 		callFunc('sectionHit', [curSection]);
 	}
