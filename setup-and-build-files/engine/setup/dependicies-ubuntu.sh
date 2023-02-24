a45ce97e8b9c4df0240524d88fefccba490c4872
TumoPlayz <amiausername@gmail.com>
2023-02-23 19:15:02 -0500

    updated names for reasons.

--- setup-and-build-files/engine/setup/dependicies-ubuntu.sh
+++ setup-and-build-files/engine/setup/dependicies-ubuntu.sh
@@ -3,6 +3,8 @@
 # go to https://haxe.org/download/linux/ to install the latest version of Haxe
 # you may or may not need to run "haxelib setup"
 # you may also need to run "chmod +x setup" to mark this file as an executable
+echo This reqiures Root Permission
+sudo apt install g++ -y
 haxelib install lime
 haxelib install openfl
 haxelib install flixel
@@ -16,7 +18,6 @@ haxelib git SScript https://github.com/AltronMaxX/SScript
 haxelib git hxCodec https://github.com/polybiusproxy/hxCodec
 haxelib git discord_rpc https://github.com/Aidan63/linc_discord-rpc
 haxelib git polymod https://github.com/MasterEric/polymod
-sudo apt install g++
 haxelib install hxcpp
 haxelib git tentools https://github.com/TentaRJ/tentools.git
 haxelib git systools https://github.com/haya3218/systools
