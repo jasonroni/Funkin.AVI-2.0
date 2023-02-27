de7f83d743f4107ae45a732f1b59f497882c2430
DEMOLITIONDON96 <87618246+DEMOLITIONDON96@users.noreply.github.com>
2023-02-27 08:56:17 -0600

    Removed a pointless argument

--- source/globals/Paths.hx
+++ source/globals/Paths.hx
@@ -310,9 +310,9 @@ class Paths
 	*
 	* @author DEMOLITIONDON96
 	*/
-	inline static public function voicesPlayer(song:String, diff:String = 'normal', singer:String = 'bf'):Any
+	inline static public function voicesPlayer(song:String, diff:String = 'normal'):Any
 	{
-		var songKey:String = '${CoolUtil.swapSpaceDash(song.toLowerCase())}/Voices-${diff}-${singer}';
+		var songKey:String = '${CoolUtil.swapSpaceDash(song.toLowerCase())}/Voices-${diff}';
 		var voices = returnSound('songs', songKey);
 		return voices;
 	}
