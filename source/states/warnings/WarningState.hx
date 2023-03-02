b156244ad482d3cff3ab47e7fc5b213c764f7331
DEMOLITIONDON96 <87618246+DEMOLITIONDON96@users.noreply.github.com>
2023-03-02 09:29:13 -0600

    Update WarningState.hx

--- source/states/warnings/WarningState.hx
+++ source/states/warnings/WarningState.hx
@@ -1,3 +1,33 @@
 package states.warnings;
 
-// we need this for later
+import base.dependency.HardcodedShaders;
+import flash.system.System;
+import flixel.FlxBasic;
+import flixel.FlxCamera;
+import flixel.FlxG;
+import flixel.FlxObject;
+import flixel.FlxSprite;
+import flixel.addons.display.FlxRuntimeShader;
+import flixel.addons.transition.FlxTransitionableState;
+import flixel.graphics.FlxGraphic;
+import flixel.group.FlxGroup.FlxTypedGroup;
+import flixel.group.FlxSpriteGroup;
+import flixel.input.keyboard.FlxKey;
+import flixel.math.FlxMath;
+import flixel.math.FlxPoint;
+import flixel.math.FlxRect;
+import flixel.system.FlxSound;
+import flixel.text.FlxText;
+import flixel.tweens.FlxEase;
+import flixel.tweens.FlxTween;
+import flixel.util.FlxColor;
+import flixel.util.FlxSort;
+import flixel.util.FlxTimer;
+import lime.app.Application;
+import openfl.media.Sound;
+import states.MusicBeatState;
+
+class FlashingState extends MusicBeatState
+{
+        // insert code here
+}
