6fe272c07581d0375b090ac560adf53b12191728
DEMOLITIONDON96 <87618246+DEMOLITIONDON96@users.noreply.github.com>
2023-03-06 10:07:11 -0600

    final version of the health system

--- source/base/utils/ScoreUtils.hx
+++ source/base/utils/ScoreUtils.hx
@@ -45,7 +45,7 @@ class ScoreUtils
 		{
 			name: "sick",
 			score: 350,
-			health: (PlayState.curStage == "waltRoom" ? 100 : 60),  // THE REASON WHY IT'S LENIENT ON MERCY IS DUE TO THE MECHANIC - don
+			health: (PlayState.curStage == "waltRoom" ? 100 : 12),  // THE REASON WHY IT'S LENIENT ON MERCY IS DUE TO THE MECHANIC - don
 			accuracy: 100,
 			timing: 45,
 			timingCap: 45,
@@ -54,7 +54,7 @@ class ScoreUtils
 		{
 			name: "good",
 			score: 150,
-			health: (PlayState.curStage == "waltRoom" ? 50 : 30),
+			health: (PlayState.curStage == "waltRoom" ? 50 : 8),
 			accuracy: 85,
 			timing: 90,
 			timingCap: 90,
@@ -63,7 +63,7 @@ class ScoreUtils
 		{
 			name: "bad",
 			score: 50,
-			health: (PlayState.curStage == "waltRoom" ? 20 : 5),
+			health: (PlayState.curStage == "waltRoom" ? 20 : 3),
 			accuracy: 50,
 			timing: 125,
 			timingCap: 125,
@@ -72,7 +72,7 @@ class ScoreUtils
 		{
 			name: "shit",
 			score: -50,
-			health: (PlayState.curStage == "waltRoom" ? -50 : -100),
+			health: (PlayState.curStage == "waltRoom" ? -50 : -150),
 			accuracy: 0,
 			timing: 150,
 			timingCap: 150,
@@ -81,7 +81,7 @@ class ScoreUtils
 		{
 			name: "miss",
 			score: -100,
-			health: (PlayState.curStage == "waltRoom" ? -100 : -160),
+			health: (PlayState.curStage == "waltRoom" ? -100 : -200),
 			accuracy: 0,
 			timing: 175,
 			timingCap: 175,
