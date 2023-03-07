0e4e473b78a214b76ec1b0610e5b3a8a2c2aea66
DEMOLITIONDON96 <87618246+DEMOLITIONDON96@users.noreply.github.com>
2023-03-07 11:07:35 -0600

    Update PlayState.hx

--- source/states/PlayState.hx
+++ source/states/PlayState.hx
@@ -2931,7 +2931,7 @@ class PlayState extends MusicBeatState
 			FlxTransitionableState.skipNextTransOut = true;
 
 			PlayState.SONG = Song.loadFromJson(song.toLowerCase() + diff, song);
-			ForeverTools.killMusic([songMusic, vocals]);
+			ForeverTools.killMusic([songMusic, songMusicNew, bf_vocals, opp_vocals, vocals]);
 
 			// deliberately did not use the main.switchstate as to not unload the assets
 			FlxG.switchState(new PlayState());
