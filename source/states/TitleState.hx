a2a2b7940b7132a6047e009c6ba945a0ea53a299
DEMOLITIONDON96 <87618246+DEMOLITIONDON96@users.noreply.github.com>
2023-03-08 09:56:17 -0600

    whoops

--- source/states/TitleState.hx
+++ source/states/TitleState.hx
@@ -58,7 +58,7 @@ class TitleState extends states.MusicBeatState
 	var blackScreen:FlxSprite;
 	var credTextShit:Alphabet;
 	var textGroup:FlxGroup;
-
+	VAR CREDgROUP:FlxGroup;
 	var curWacky:Array<String> = [];
 
 	var wackyImage:FlxSprite;
@@ -179,9 +179,9 @@ class TitleState extends states.MusicBeatState
 
 	override public function create():Void
 	{	
-		if (FlxG.save.data.hasSeenWarning == null) Main.switchState(this, states.warnings.WarningState());
+		//if (FlxG.save.data.hasSeenWarning == null) Main.switchState(this, states.warnings.WarningState());
 		
-		if (FlxG.save.data.hasSeenDisclaimer == null) Main.switchState(this, states.warnings.DisclaimerState()); // you never know...
+		//if (FlxG.save.data.hasSeenDisclaimer == null) Main.switchState(this, states.warnings.DisclaimerState()); // you never know...
 
 		if (FlxG.save.data.episode1FPLock == null || FlxG.save.data.episodeSFPLock == null /*an alternative in case we don't make a new save for the testers*/) GameData.lockinIt(); // this locks everything to the default state if the player hasn't played it yet, leaving only Episode 1 unlocked
 
