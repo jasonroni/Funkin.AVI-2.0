cf506c91f3ed2c55bb0c95b285c68d08e3eaa487
DEMOLITIONDON96 <87618246+DEMOLITIONDON96@users.noreply.github.com>
2023-03-20 09:54:40 -0500

    might as well until we get the new freeplay menu UI

--- source/states/menus/freeplay/FreeplayState.hx
+++ source/states/menus/freeplay/FreeplayState.hx
@@ -432,9 +432,13 @@ class FreeplayState extends MusicBeatState
 		var bullShit:Int = 0;
 
 		for (i in 0...iconArray.length)
+		{
 			iconArray[i].alpha = 0.6;
+			iconArray[i].animation.curAnim.curFrame = 0;
+		}
 
 		iconArray[curSelected].alpha = 1;
+		iconArray[curSelected].animation.curAnim.curFrame = 2;
 
 		for (item in grpSongs.members)
 		{
