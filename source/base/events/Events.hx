cb536b921a7dee5cad08830749f834c48f2884e9
JustJasonLol <98182734+JustJasonLol@users.noreply.github.com>
2023-04-04 14:40:44 -0300

    later on

---
+++ source/base/events/Events.hx
@@ -0,0 +1,16 @@
+package base.events;
+
+import flixel.group.FlxGroup.FlxTypedGroup;
+import flixel.FlxObject;
+import flixel.util.FlxColor;
+import flixel.FlxG;
+import flixel.FlxBasic;
+import flixel.text.FlxText;
+import flixel.FlxSprite;
+import flixel.tweens.FlxTween;
+import flixel.tweens.FlxEase;
+
+class IsolatedEvents extends FlxTypedGroup<Dynamic> 
+{
+    
+}
