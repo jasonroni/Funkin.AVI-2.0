435ad040bdbca1013a3f504057c779cea12d9e3b
DEMOLITIONDON96 <87618246+DEMOLITIONDON96@users.noreply.github.com>
2023-04-13 09:15:09 -0500

    changes

--- source/states/PlayState.hx
+++ source/states/PlayState.hx
@@ -1597,16 +1597,19 @@ class PlayState extends MusicBeatState
 								{
 									addShaderToCamera('hud', new MalfunctionLegacyEffect(0.01));
 									addShaderToCamera('notes', new MalfunctionLegacyEffect(0.01));
-									addShaderToCamera('game', new MalfunctionNewEffect(0.01));
+									addShaderToCamera('game', new MalfunctionNewEffect(0.025, 0.01));
 								}
 								new FlxTimer().start(0.04, function(tmr:FlxTimer)
 								{
 									clearShaderFromCamera('game');
 									clearShaderFromCamera('notes');
 									clearShaderFromCamera('hud');
-									addShaderToCamera('hud', new MalfunctionLegacyEffect(0.004));
-									addShaderToCamera('notes', new MalfunctionLegacyEffect(0.004));
-									addShaderToCamera('game', new MalfunctionNewEffect(0.005));
+									if(!Init.trueSettings.get('Low Quality'))
+									{
+										addShaderToCamera('hud', new MalfunctionLegacyEffect(0.004));
+										addShaderToCamera('notes', new MalfunctionLegacyEffect(0.004));
+										addShaderToCamera('game', new MalfunctionNewEffect(0.01, 0.005));
+									}
 								});
 							}
 						}
