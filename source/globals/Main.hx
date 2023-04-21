66c9bd39672388fef3e1b65d4efafa7cc9cdb9ea
DEMOLITIONDON96 <87618246+DEMOLITIONDON96@users.noreply.github.com>
2023-04-21 10:48:29 -0500

    praying I can get the game to create a new crash dialog system soon

--- source/globals/Main.hx
+++ source/globals/Main.hx
@@ -392,7 +392,7 @@ class Main extends Sprite
 		dateNow = StringTools.replace(dateNow, " ", "_");
 		dateNow = StringTools.replace(dateNow, ":", "'");
 
-		path = "crash/" + "Feather_" + dateNow + ".txt";
+		path = "crash/" + "F.AVI_" + dateNow + ".txt";
 
 		for (stackItem in callStack)
 		{
@@ -406,7 +406,7 @@ class Main extends Sprite
 			}
 		}
 
-		errMsg += "\nUncaught Error: " + e.error + " - Please report this error to the\nGitHub page https://github.com/BeastlyGhost/Forever-Engine-Feather";
+		errMsg += "\nUncaught Error: " + e.error + " - Please report this error to the\nDunkin Funkin community server";
 
 		if (!FileSystem.exists("crash/"))
 			FileSystem.createDirectory("crash/");
@@ -416,7 +416,7 @@ class Main extends Sprite
 		Sys.println(errMsgPrint);
 		Sys.println("Crash dump saved in " + Path.normalize(path));
 
-		var crashDialoguePath:String = "FEF-CrashDialog";
+		var crashDialoguePath:String = "FAVI-CrashDialog";
 
 		#if windows
 		crashDialoguePath += ".exe";
