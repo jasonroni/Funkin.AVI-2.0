6d84c3087df8d44eacc1b8e64367855d9d9463fa
JustJasonLol <98182734+JustJasonLol@users.noreply.github.com>
2023-04-30 13:18:04 -0300

    fixed upscroll input

--- source/objects/ui/notes/Notefield.hx
+++ source/objects/ui/notes/Notefield.hx
@@ -43,7 +43,7 @@ class Notefield extends FlxTypedGroup<Note>
 	public function noteCalls(daNote:Note, strumline:Strumline)
 	{
 		// set the notes x and y
-		var downscrollMultiplier:Float = (strumline.downscroll ? -1: 1) * FlxMath.signOf(PlayState.songSpeed);
+		var downscrollMultiplier:Float = (strumline.downscroll ? -1: 0.73) * FlxMath.signOf(PlayState.songSpeed);
 
 		var roundedSpeed = FlxMath.roundDecimal(daNote.noteSpeed, 2);
 		var receptorPosX:Float = strumline.receptors.members[Math.floor(daNote.noteData)].x;
