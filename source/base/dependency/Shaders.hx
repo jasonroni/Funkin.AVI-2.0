cfb134d1e90df8cb8bc90be9b7a9cc4eb7fe9386
FuckingAltLol <95931389+FuckingAltLol@users.noreply.github.com>
2023-06-30 11:33:20 -0500

    i pasted the wrong frag file lmfao

--- source/base/dependency/Shaders.hx
+++ source/base/dependency/Shaders.hx
@@ -1211,45 +1211,14 @@ enum abstract Shaders(String) from String to String
 
 	#define PI 3.14159265
 	uniform float time = 0.0;
-	uniform float intensity = 0.0;
-	uniform float initial = 0.0;
-	
-	float sat( float t ) {
-		return clamp( t, 0.0, 1.0 );
-	}
-	
-	vec2 sat( vec2 t ) {
-		return clamp( t, 0.0, 1.0 );
-	}
-	
-	vec3 spectrum_offset( float t ) {
-	    float t0 = 3.0 * t - 1.5;
-		return clamp( vec3( -t0, 1.0-abs(t0), t0), 0.0, 1.0);
-	}
+	uniform float vignetteIntensity = 0.75;
 	
 	void main() {
-	    vec2 uv = openfl_TextureCoordv;
-	    float ofs = (initial / 1000) + (intensity / 1000);
-	
-		vec4 sum = vec4(0.0);
-		vec3 wsum = vec3(0.0);
-	    const int samples = 4;
-	    const float sampleinverse = 1.0 / float(samples);
-		for( int i=0; i<samples; ++i )
-		{
-	        float t = float(i) * sampleinverse;
-			uv.x = sat( uv.x + ofs * t );
-			vec4 samplecol = texture2D( bitmap, uv, -10.0 );
-			vec3 s = spectrum_offset( t );
-			samplecol.rgb = samplecol.rgb * s;
-			sum += samplecol;
-			wsum += s;
-	    }
-		sum.rgb /= wsum;
-		sum.a *= sampleinverse;
-	    
-		gl_FragColor.a = sum.a;
-		gl_FragColor.rgb = sum.rgb; 
+	    float amount = (0.25 * sin(time * PI) + vignetteIntensity);
+	    vec4 color = texture2D(bitmap, openfl_TextureCoordv);
+	    float vignette = distance(openfl_TextureCoordv, vec2(0.5));
+	    vignette = mix(1.0, 1.0 - amount, vignette);
+		gl_FragColor = vec4(mix(vec3(1.0, 0.0, 0.0), color.rgb, vignette), 1.0 - vignette);
 	}
     ";
 
