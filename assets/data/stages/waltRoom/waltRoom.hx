ab973571ae0e4e6daaa79fd6c987efd423e10592
DEMOLITIONDON96 <87618246+DEMOLITIONDON96@users.noreply.github.com>
2023-08-03 16:36:34 -0500

    reverted some stuff i didn't mean to change
    i fucked up i fucked up i fucked up i fucked up i fucked up i fucked up i fucked up i fucked up i fucked up i fucked up i fucked up i fucked up i fucked up i fucked up
     i fucked up i fucked up i fucked up i fucked up i fucked up i fucked up i fucked up
     i fucked up i fucked up i fucked up i fucked up i fucked up i fucked up i fucked up
     i fucked up i fucked up i fucked up i fucked up i fucked up i fucked up i fucked up
     i fucked up i fucked up i fucked up i fucked up i fucked up i fucked up i fucked up
     i fucked up i fucked up i fucked up i fucked up i fucked up i fucked up i fucked up

--- assets/data/stages/waltRoom/waltRoom.hx
+++ assets/data/stages/waltRoom/waltRoom.hx
@@ -48,9 +48,3 @@ function onCreate()
 			add(vignette);
 		}
 }
-
-function charStagePos(boyfriend:Character, gf:Character, dad:Character)
-{
-	dad.setPosition(0, 0);
-    	boyfriend.setPosition(330, 300); //make sure to replace bf with a first-person pov variant
-}
